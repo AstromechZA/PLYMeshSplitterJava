7f69b84f5e8bb92a253a4897ec53f1e0d8bf3cf4
Ben Meier <benmeier42@gmail.com>
2014-06-03 01:08:11 +0200

    Pulled method out to Main

--- src/org/uct/cs/simplify/Main.java
+++ src/org/uct/cs/simplify/Main.java
@@ -1,6 +1,8 @@
 package org.uct.cs.simplify;
 
 import org.uct.cs.simplify.ply.reader.PLYReader;
+import org.uct.cs.simplify.ply.reader.Vertex;
+import org.uct.cs.simplify.ply.reader.VertexReader;
 import org.uct.cs.simplify.util.Timer;
 
 import java.io.File;
@@ -17,6 +19,43 @@ public class Main
         {
 
             PLYReader r = new PLYReader(new File(filename));
+
+            try (Timer t = new Timer("v reader");
+                 VertexReader vr = new VertexReader(r.getFile(), r.getPositionOfElement("vertex"), r.getHeader().getElement("vertex").getCount(), 20))
+            {
+                Vertex v;
+                int n = 0;
+                float minx = Float.MAX_VALUE,
+                        maxx = -Float.MAX_VALUE,
+                        miny = Float.MAX_VALUE,
+                        maxy = -Float.MAX_VALUE,
+                        minz = Float.MAX_VALUE,
+                        maxz = -Float.MAX_VALUE;
+
+                while (vr.hasNext())
+                {
+                    n += 1;
+                    v = vr.next();
+
+                    minx = Math.min(minx, v.x);
+                    maxx = Math.max(maxx, v.x);
+
+                    miny = Math.min(miny, v.y);
+                    maxy = Math.max(maxy, v.y);
+
+                    minz = Math.min(minz, v.z);
+                    maxz = Math.max(maxz, v.z);
+                }
+                System.out.printf("x: %f  %f  %f\n", minx, maxx, (minx + maxx) / 2);
+                System.out.printf("y: %f  %f  %f\n", miny, maxy, (miny + maxy) / 2);
+                System.out.printf("z: %f  %f  %f\n", minz, maxz, (minz + maxz) / 2);
+
+
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
 
         }
         catch (IOException e)
--- src/org/uct/cs/simplify/ply/reader/PLYReader.java
+++ src/org/uct/cs/simplify/ply/reader/PLYReader.java
@@ -1,7 +1,6 @@
 package org.uct.cs.simplify.ply.reader;
 
 import org.uct.cs.simplify.ply.header.*;
-import org.uct.cs.simplify.util.Timer;
 
 import java.io.File;
 import java.io.IOException;
@@ -20,41 +19,6 @@ public class PLYReader
 
         this.elementPositions = calculateElementPositionsNaive();
 
-        try (Timer t = new Timer("v reader"); VertexReader vr = new VertexReader(f, elementPositions.get("vertex"), this.header.getElement("vertex").getCount(), 20))
-        {
-            Vertex v;
-            int n = 0;
-            float minx = Float.MAX_VALUE,
-                    maxx = -Float.MAX_VALUE,
-                    miny = Float.MAX_VALUE,
-                    maxy = -Float.MAX_VALUE,
-                    minz = Float.MAX_VALUE,
-                    maxz = -Float.MAX_VALUE;
-
-            while (vr.hasNext())
-            {
-                n += 1;
-                v = vr.next();
-
-                minx = Math.min(minx, v.x);
-                maxx = Math.max(maxx, v.x);
-
-                miny = Math.min(miny, v.y);
-                maxy = Math.max(maxy, v.y);
-
-                minz = Math.min(minz, v.z);
-                maxz = Math.max(maxz, v.z);
-            }
-            System.out.printf("x: %f  %f  %f\n", minx, maxx, (minx + maxx) / 2);
-            System.out.printf("y: %f  %f  %f\n", miny, maxy, (miny + maxy) / 2);
-            System.out.printf("z: %f  %f  %f\n", minz, maxz, (minz + maxz) / 2);
-
-
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
 
     }
 
@@ -113,4 +77,8 @@ public class PLYReader
     }
 
 
+    public File getFile()
+    {
+        return file;
+    }
 }
