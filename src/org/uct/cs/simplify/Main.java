4b4584636af807fe20de840ce6afafe74428e39d
Ben Meier <benmeier42@gmail.com>
2014-06-03 23:46:22 +0200

    Removed currently unneccessary code

--- src/org/uct/cs/simplify/Main.java
+++ src/org/uct/cs/simplify/Main.java
@@ -1,13 +1,8 @@
 package org.uct.cs.simplify;
 
-import org.uct.cs.simplify.img.BluePrintGenerator;
 import org.uct.cs.simplify.ply.reader.PLYReader;
-import org.uct.cs.simplify.util.MemRecorder;
 import org.uct.cs.simplify.util.Timer;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -19,27 +14,15 @@ public class Main
         String filename = "C:\\Users\\Ben\\Desktop\\Chapel_of_Nossa_Senhora_de_Baluarte_ao.ply";
         //String filename = "C:\\Users\\Ben\\Desktop\\Gede Palace _3_Mio.ply";
 
-        try (MemRecorder mr = new MemRecorder(new File("C:\\Users\\Ben\\o.dat"), 100); Timer ignored = new Timer("Entire read"))
+        try (Timer ignored = new Timer("Entire read"))
         {
 
             PLYReader r = new PLYReader(new File(filename));
 
-            BufferedImage xy = BluePrintGenerator.CreateImage(r, 1024, 0.1f, BluePrintGenerator.Axis.X_Y);
-            BufferedImage xz = BluePrintGenerator.CreateImage(r, 512, 0.02f, BluePrintGenerator.Axis.X_Z);
-            BufferedImage yz = BluePrintGenerator.CreateImage(r, 512, 0.02f, BluePrintGenerator.Axis.Y_Z);
-
-            BufferedImage bi = new BufferedImage(1024 + 512, 1024, BufferedImage.TYPE_INT_RGB);
-            Graphics2D g = (Graphics2D) bi.getGraphics();
-            g.drawImage(xy, 0, 0, null);
-            g.drawImage(xz, 1024, 0, null);
-            g.drawImage(yz, 1024, 512, null);
-
-            ImageIO.write(bi, "jpg", new File("C:\\Users\\Ben\\o.jpg"));
-
             System.out.printf("%d vertices\n", r.getHeader().getElement("vertex").getCount());
 
         }
-        catch (IOException | InterruptedException e)
+        catch (IOException e)
         {
             e.printStackTrace();
         }
