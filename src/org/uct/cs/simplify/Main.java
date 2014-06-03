18f86a052f93a212d9ce5a1ce91c12b35b6c622b
Ben Meier <benmeier42@gmail.com>
2014-06-03 21:24:55 +0200

    Remove old code, leave just the blueprint stuff

--- src/org/uct/cs/simplify/Main.java
+++ src/org/uct/cs/simplify/Main.java
@@ -2,8 +2,6 @@ package org.uct.cs.simplify;
 
 import org.uct.cs.simplify.img.BluePrintGenerator;
 import org.uct.cs.simplify.ply.reader.PLYReader;
-import org.uct.cs.simplify.ply.reader.Vertex;
-import org.uct.cs.simplify.ply.reader.VertexReader;
 import org.uct.cs.simplify.util.Timer;
 
 import javax.imageio.ImageIO;
@@ -26,45 +24,6 @@ public class Main
 
             System.out.printf("%d vertices\n", r.getHeader().getElement("vertex").getCount());
 
-            try (VertexReader vr = new VertexReader(r.getFile(), r.getPositionOfElement("vertex"), r.getHeader().getElement("vertex").getCount(), 20))
-            {
-                Vertex v;
-                int n = 0;
-                float minx = Float.MAX_VALUE,
-                        maxx = -Float.MAX_VALUE,
-                        miny = Float.MAX_VALUE,
-                        maxy = -Float.MAX_VALUE,
-                        minz = Float.MAX_VALUE,
-                        maxz = -Float.MAX_VALUE;
-
-                long s = System.nanoTime();
-                while (vr.hasNext())
-                {
-                    n += 1;
-                    v = vr.next();
-
-                    minx = Math.min(minx, v.x);
-                    maxx = Math.max(maxx, v.x);
-
-                    miny = Math.min(miny, v.y);
-                    maxy = Math.max(maxy, v.y);
-
-                    minz = Math.min(minz, v.z);
-                    maxz = Math.max(maxz, v.z);
-                }
-                long e = System.nanoTime();
-                System.out.printf("%f vertices per second.\n", (float) (e - s) / n);
-                System.out.printf("x: %f < %f @ %f\n", minx, maxx, (minx + maxx) / 2);
-                System.out.printf("y: %f < %f @ %f\n", miny, maxy, (miny + maxy) / 2);
-                System.out.printf("z: %f < %f @ %f\n", minz, maxz, (minz + maxz) / 2);
-
-
-            }
-            catch (Exception e)
-            {
-                e.printStackTrace();
-            }
-
             try
             {
                 BufferedImage bi = BluePrintGenerator.CreateImage(r, 1024, 0.1f, BluePrintGenerator.Axis.X_Z);
@@ -74,7 +33,6 @@ public class Main
             {
                 e.printStackTrace();
             }
-
 
         }
         catch (IOException e)
