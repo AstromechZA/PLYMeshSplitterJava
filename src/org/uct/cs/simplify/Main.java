36f412d5829be24af6ecdfe9d1298ba3d0d8e65a
Ben Meier <benmeier42@gmail.com>
2014-06-04 20:05:18 +0200

    Added command line parsing & simplfied in prep for v2 reader

--- src/org/uct/cs/simplify/Main.java
+++ src/org/uct/cs/simplify/Main.java
@@ -1,6 +1,7 @@
 package org.uct.cs.simplify;
 
-import org.uct.cs.simplify.ply.reader.*;
+import org.apache.commons.cli.*;
+import org.uct.cs.simplify.ply.reader.PLYReader;
 import org.uct.cs.simplify.util.MemRecorder;
 import org.uct.cs.simplify.util.Timer;
 
@@ -12,37 +13,13 @@ public class Main
 
     public static void main(String[] args)
     {
-        String filename = "C:\\Users\\Ben\\Desktop\\Chapel_of_Nossa_Senhora_de_Baluarte_ao.ply";
-        //String filename = "C:\\Users\\Ben\\Desktop\\Gede Palace _3_Mio.ply";
+        CommandLine cmd = getCommandLine(args);
 
         try (MemRecorder m = new MemRecorder(new File("C:\\Users\\Ben\\o.dat"), 50); Timer ignored = new Timer("Entire read"))
         {
-
-            PLYReader r = new PLYReader(new File(filename));
-            int c = r.getHeader().getElement("vertex").getCount();
-            long p = r.getPositionOfElement("vertex");
-
-            try (MemoryMappedVertexReader mmvr = new MemoryMappedVertexReader(r.getFile(), p, c, 20))
-            {
-                int fc = r.getHeader().getElement("face").getCount();
-                long fp = r.getPositionOfElement("face");
-                long fl = r.getLengthOfElement("face");
-
-                try (MemoryMappedFaceReader fr = new MemoryMappedFaceReader(r.getFile(), fp, fc, fl))
-                {
-                    Face face;
-                    while (fr.hasNext())
-                    {
-                        face = fr.next();
-                        for (Integer i : face.getVertices())
-                        {
-                            Vertex v = mmvr.get(i);
-                        }
-                    }
-                }
-            }
-
-            System.out.printf("%d vertices\n", r.getHeader().getElement("vertex").getCount());
+            // == construct & setup PLYReader
+            // this scans the target file and works out start and end ranges
+            PLYReader r = new PLYReader(new File(cmd.getOptionValue("f")));
 
         }
         catch (IOException | InterruptedException e)
@@ -50,4 +27,38 @@ public class Main
             e.printStackTrace();
         }
     }
+
+    private static CommandLine getCommandLine(String[] args)
+    {
+        CommandLineParser clp = new BasicParser();
+
+        Options options = new Options();
+
+        Option o1 = new Option("f", "filename", true, "path to PLY model to process");
+        o1.setRequired(true);
+        options.addOption(o1);
+
+        Option o2 = new Option("d", "depth", true, "number of levels to split to");
+        o2.setType(Short.class);
+        options.addOption(o2);
+
+        Option o3 = new Option("s", "scaleTo", true, "scale the model to fit a cube of the given size");
+        o3.setType(Short.class);
+        options.addOption(o3);
+
+        CommandLine cmd;
+        try
+        {
+            cmd = clp.parse(options, args);
+            return cmd;
+        }
+        catch (ParseException e)
+        {
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("PLYMeshSplitterJ --filename <path>", options);
+            System.exit(1);
+            return null;
+        }
+    }
+
 }
