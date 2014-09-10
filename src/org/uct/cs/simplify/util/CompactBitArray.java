e3fddcc8b4d13618a48c96f421b799004f7377e4
Ben Meier <benmeier42@gmail.com>
2014-09-10 20:56:14 +0200

    Created CompactBitArray

---
+++ src/org/uct/cs/simplify/util/CompactBitArray.java
@@ -0,0 +1,75 @@
+package org.uct.cs.simplify.util;
+
+public class CompactBitArray
+{
+    private static final int INTEGER_SIZE = 32;
+
+    private final int bits;
+    private final long length;
+    private final int blockLength;
+    private final int blocksPerInt;
+    private final int[] data;
+    private final int bitMask;
+
+    public CompactBitArray(int bits, long length)
+    {
+        // for compactness and performance reasons we only support blocks of size 1, 2, 4, 8 bits. For this reason,
+        // <bits> should be smaller than or equal to 8
+        if (bits < 1 || bits > 8)
+            throw new IllegalArgumentException("Bits argument must be between 1 and 8 (inclusive).");
+        this.bits = bits;
+        this.length = length;
+        this.blockLength = (int) Math.pow(2, Math.ceil(Math.log(bits) / Math.log(2)));
+        this.blocksPerInt = INTEGER_SIZE / this.blockLength;
+
+        // now we setup
+        int numBlocks = (int) Math.ceil((float) this.blockLength * this.length / INTEGER_SIZE);
+        this.data = new int[ numBlocks ];
+        this.bitMask = (1 << this.bits) - 1;
+    }
+
+    public CompactBitArray(int length)
+    {
+        this(1, length);
+    }
+
+    public long size()
+    {
+        return this.length;
+    }
+
+    public int getBits()
+    {
+        return this.bits;
+    }
+
+    public int get(long i)
+    {
+        // first identify which index we need
+        long blockIndex = i / this.blocksPerInt;
+        if (blockIndex >= this.data.length) throw new IndexOutOfBoundsException();
+        int block = this.data[ (int) blockIndex ];
+
+        // get positional index
+        int positionIndex = (int) (i % this.blocksPerInt) * this.blockLength;
+        return (block >> positionIndex) & this.bitMask;
+    }
+
+    public void set(long i, int v)
+    {
+        // first identify which index we need
+        long blockIndex = i / this.blocksPerInt;
+        if (blockIndex >= this.data.length) throw new IndexOutOfBoundsException();
+        int block = this.data[ (int) blockIndex ];
+
+        // get positional index
+        int positionIndex = (int) (i % this.blocksPerInt) * this.blockLength;
+
+        int wiper = this.bitMask << positionIndex;
+        int oldData = ~((~block) | wiper);
+        int newData = (v & this.bitMask) << positionIndex;
+        this.data[ (int) blockIndex ] = oldData | newData;
+    }
+
+
+}
