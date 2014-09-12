d1e39f0f80feb5b6989254794795996e203b3d83
Ben Meier <benmeier42@gmail.com>
2014-09-12 14:18:39 +0200

    Started KDTree builder

---
+++ src/org/uct/cs/simplify/splitter/memberships/KDTreeMembershipBuilder.java
@@ -0,0 +1,15 @@
+package org.uct.cs.simplify.splitter.memberships;
+
+import org.uct.cs.simplify.ply.reader.PLYReader;
+import org.uct.cs.simplify.util.XBoundingBox;
+
+import java.io.IOException;
+
+public class KDTreeMembershipBuilder implements IMembershipBuilder
+{
+    @Override
+    public MembershipBuilderResult build(PLYReader reader, XBoundingBox boundingBox) throws IOException
+    {
+        return null;
+    }
+}
