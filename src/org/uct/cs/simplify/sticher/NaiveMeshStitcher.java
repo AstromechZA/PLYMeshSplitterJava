76d7c7a8961ab71dba699cac11039b5a11fdcb3c
Ben Meier <benmeier42@gmail.com>
2014-09-13 16:37:59 +0200

    Added commented stitcher structure

--- src/org/uct/cs/simplify/sticher/NaiveMeshStitcher.java
+++ src/org/uct/cs/simplify/sticher/NaiveMeshStitcher.java
@@ -8,6 +8,30 @@ public class NaiveMeshStitcher
 {
     public static PLYHeader stitch(File file1, File file2, File outputFile)
     {
+        // load in mesh1
+        // create TObjectIntHashMap W (detect if vertices are already added, and what they're indexed to)
+
+        // insert all of mesh1's vertices into W with incremening indices
+
+        // write vertices to tempfile1
+        // write faces to tempfile2
+
+        // create TIntIntHashMap G
+
+        // run through all vertices
+        // if vertex in W
+        // put transformed index into G
+        // else
+        // increment I, store in G,
+        // write vertex into tempfile1
+
+        // run through faces
+        // for each vertex
+        // pull new index from G
+        // write to tempfile2
+
+        // combine tempfiles into final file
+
         return null;
     }
 }
