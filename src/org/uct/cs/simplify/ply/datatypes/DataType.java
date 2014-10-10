837a60367354111ecc2f844ffdfd7e6cad9f854e
Ben Meier <benmeier42@gmail.com>
2014-10-10 17:41:24 +0200

    Fix for additional datatypes

--- src/org/uct/cs/simplify/ply/datatypes/DataType.java
+++ src/org/uct/cs/simplify/ply/datatypes/DataType.java
@@ -1,5 +1,7 @@
 package org.uct.cs.simplify.ply.datatypes;
 
+import java.util.HashMap;
+
 public enum DataType
 {
     CHAR("char", 1),
@@ -10,7 +12,30 @@ public enum DataType
     UINT("uint", 4),
     FLOAT("float", 4),
     DOUBLE("double", 8);
+    private static final HashMap<String, DataType> translator;
 
+    static
+    {
+        translator = new HashMap<>();
+        translator.put("CHAR", CHAR);
+        translator.put("UCHAR", UCHAR);
+        translator.put("SHORT", SHORT);
+        translator.put("USHORT", USHORT);
+        translator.put("INT", INT);
+        translator.put("UINT", UINT);
+        translator.put("FLOAT", FLOAT);
+        translator.put("DOUBLE", DOUBLE);
+        translator.put("CHAR", CHAR);
+
+        translator.put("INT8", CHAR);
+        translator.put("UINT8", UCHAR);
+        translator.put("INT16", SHORT);
+        translator.put("UINT16", USHORT);
+        translator.put("INT32", INT);
+        translator.put("UINT32", UINT);
+        translator.put("FLOAT32", FLOAT);
+        translator.put("FLOAT64", DOUBLE);
+    }
     private final String name;
     private final int bytes;
 
@@ -22,7 +47,7 @@ public enum DataType
 
     public static DataType from(String s)
     {
-        return valueOf(s.trim().toUpperCase());
+        return translator.get(s.trim().toUpperCase());
     }
 
     public int getByteSize()
